bb8a2660c4743d6f1af25ea5b24a0e1e3fc26731
arvindshekar07 <arvindrocks07@gmail.com>
2015-04-29 09:17:48 +0000

    final

--- worker.sh
+++ worker.sh
@@ -1,5 +1,5 @@
 sudo ldconfig
-$HOME/CloudKonPlus/src/ckp_worker -z $HOME/CloudKonPlus/src/zht.conf -n $HOME/CloudKonPlus/src/neighbor.conf -t 2 
+$HOME/CloudKonPlus/src/ckp_worker -z $HOME/CloudKonPlus/src/zht.conf -n $HOME/CloudKonPlus/src/neighbor.conf -t 1 
 #./ckp_worker -z zht.conf -n neighbor.conf -t #numThreads
 #./ckp_worker -z zht.conf -n neighbor.conf -t #numThreads
 #$HOME/ZHT-master-FaBRiQ-mod/src/ckp_client -z $HOME/ZHT-master-FaBRiQ-mod/src/zht.conf -n $HOME/ZHT-master-FaBRiQ-mod/src/neighbor.conf -w $HOME/ZHT-master-FaBRiQ-mod/src/TestCase.txt &
