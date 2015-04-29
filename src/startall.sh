d4607c67b500fd4673ff75a5b6381fd24a566065
Ubuntu <ubuntu@ip-172-31-37-85.us-west-2.compute.internal>
2015-04-29 02:22:31 +0000

    lol _lol

--- src/startall.sh
+++ src/startall.sh
@@ -1,8 +1,4 @@
-./zhtserver -z zht.conf -n neighbor2.conf -p 50001 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50002 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50003 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50004 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50005 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50006 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50007 & sleep 1
-./zhtserver -z zht.conf -n neighbor2.conf -p 50008 & sleep 1
+./zhtserver -z zht.conf -n neighbor.conf -p 50000 & sleep 1
+./zhtserver -z zht.conf -n neighbor.conf -p 60000 & sleep 1
+./zhtserver -z zht.conf -n neighbor.conf -p 70000 & sleep 1
+./zhtserver -z zht.conf -n neighbor.conf -p 80000 & sleep 1
