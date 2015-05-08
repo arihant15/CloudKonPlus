fd248b8d426ce1efb3bdb62b26b0360313309859
arihant15 <arihantraj@gmail.com>
2015-05-08 17:22:46 -0500

    script update
    script update

--- script/start.sh
+++ script/start.sh
@@ -1,8 +1,8 @@
 sudo ldconfig
 
-fusionfs fusion_root fusion_mount $HOME/CloudKonPlus/conf/neighbor.conf  $HOME/CloudKonPlus/conf/zht.conf &
-echo "FusionFS Started started"
-sleep 0
+#fusionfs fusion_root fusion_mount $HOME/CloudKonPlus/conf/neighbor.conf  $HOME/CloudKonPlus/conf/zht.conf &
+#echo "FusionFS Started started"
+#sleep 0
 $HOME/CloudKonPlus/src/cloudkonplus/ckp_worker -n $HOME/CloudKonPlus/conf/neighbor.conf -z $HOME/CloudKonPlus/conf/zht.conf -t 1 &
 echo "CKP Worker Started started"
 sleep 0
